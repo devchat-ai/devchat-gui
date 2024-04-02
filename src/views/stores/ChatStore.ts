b544d7d56bb5fac46d6d1acf6681139099de2c20
小石头 <debin.wang@merico.dev>
2024-04-02 16:34:33 +0800

    Update ChatStore message content

--- src/views/stores/ChatStore.ts
+++ src/views/stores/ChatStore.ts
@@ -213,9 +213,7 @@ Your DevChat Access Key is not detected in the current settings. Please set your
         self.messages.push(
           Message.create({
             type: "bot",
-            message: `# 你好
-![图片](https://deploy-script.merico.cn/devchat/workflow/readme_docstring.gif)
-            `,
+            message: helps,
           })
         );
       }
