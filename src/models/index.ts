d1a5c1fec91fc8b7c7c8e09369dd7c5b2f51c219
bobo <bobo.yang@merico.dev>
2024-04-29 11:04:38 +0800

    Update modelsTemplate in src/models/index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,76 +1,10 @@
 const modelsTemplate = [
   {
-    name: "gpt-3.5-turbo",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 13000,
-  },
-  {
-    name: "gpt-4",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 6000,
-  },
-  {
-    name: "gpt-4-turbo-preview",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 32000,
-  },
-  {
-    name: "claude-3-opus",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 32000,
-  },
-  {
-    name: "claude-3-sonnet",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 32000,
-  },
-  {
-    name: "xinghuo-3.5",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 6000,
-  },
-  {
-    name: "GLM-4",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 8000,
-  },
-  {
-    name: "ERNIE-Bot-4.0",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 8000,
-  },
-  {
-    name: "togetherai/codellama/CodeLlama-70b-Instruct-hf",
+    name: "ollama/mixtral:8x7b",
     provider: "devchat",
     stream: true,
     max_input_tokens: 4000,
-  },
-  {
-    name: "togetherai/mistralai/Mixtral-8x7B-Instruct-v0.1",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 4000,
-  },
-  {
-    name: "minimax/abab6-chat",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 4000,
-  },
-  {
-    name: "llama-2-70b-chat",
-    provider: "devchat",
-    stream: true,
-    max_input_tokens: 4000,
-  },
+  }
 ];
 
 export default modelsTemplate;
