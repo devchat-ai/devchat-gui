b3a18fe429951d0b193b3a7c0846f8a665d1ad2a
Luo Tim <shiwen@merico.dev>
2024-06-07 17:07:15 +0800

    Create message with model info

--- src/views/stores/ChatStore.ts
+++ src/views/stores/ChatStore.ts
@@ -185,7 +185,7 @@ export const ChatStore = types
         parent_hash: lastNonEmptyHash(),
         model: chatModel,
       });
-      APIUtil.createMessage({content: text});
+      APIUtil.createMessage({content: text, model: chatModel});
     };
 
     const helpMessage = (originalMessage = false) => {
