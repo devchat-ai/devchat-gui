6bcaee566c3422bd720edbc2f0f4d644d078b2c8
bobo <bobo.yang@merico.dev>
2024-07-04 12:55:59 +0800

    feat: Add Edit and Apply button for code blocks
    - Implement new EditApplyButton component in CodeButtons
    - Add callService method to IDEServiceUtil for API interactions
    - Include EditApplyButton in CodeButtons component render

--- src/util/IDEServiceUtil.ts
+++ src/util/IDEServiceUtil.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+interface ServiceResponse {
+  result?: any;
+  error?: string;
+}
+
 class IDEServiceUtil {
   private static instance: IDEServiceUtil;
   private host = "http://localhost";
@@ -31,6 +36,35 @@ class IDEServiceUtil {
       return info;
     } catch(err) {
       console.error(err);
+    }
+  }
+
+  async callService(serviceName: string, data: Record<string, any>): Promise<any> {
+    console.log("callService: ", serviceName, data);
+    try {
+      const url = `${this.host}:${this.port}/${serviceName}`;
+      console.log("callService url: ", url);
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const headers = { 'Content-Type': 'application/json' };
+  
+      const response = await axios.post<ServiceResponse>(url, data, { headers });
+      console.log("callService response: ", response);
+  
+      if (response.status !== 200) {
+        console.log(`Server error: ${response.status}`);
+        return undefined;
+      }
+  
+      const responseData = response.data;
+      if (responseData.error) {
+        console.log(`Server returned error: ${responseData.error}`);
+        return undefined;
+      }
+  
+      return responseData.result;
+    } catch (error) {
+      console.error('Error calling service:', error);
+      return undefined;
     }
   }
 }
